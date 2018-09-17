ff466c544e094757f3e599ffcb06c4f1c52eedb4
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-17 09:15:29 -0500

    Added SpaService

--- Chapter3/src/youDoit/SpaService.java
+++ Chapter3/src/youDoit/SpaService.java
@@ -5,8 +5,28 @@ public class SpaService {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-
+		 private String serviceDescription;
+		   private double price;
+		   public SpaService()
+	 {
+		 serviceDescription = "XXX";
+		  price = 0;
+	 }
+	 public void setServiceDescription(String service)
+	 {
+		 serviceDescription = service;
+	 }
+	 public void setPrice(double pr)
+	 {
+		 price = pr;
+	 }
+	 public String getServiceDescription()
+	 {
+		return serviceDescription;
+     }
+	 public double getPrice()
+	 {
+		 return price;
 	}
 
 }
